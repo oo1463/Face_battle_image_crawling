1f55c738a64ff662fac594fb2b572ebd204e648d
Yujun Jang <oooo1463@gmail.com>
2021-01-10 11:35:28 +0900

    before pull

--- image_preprocess/image_crawling_multiprocessing.py
+++ image_preprocess/image_crawling_multiprocessing.py
@@ -1,8 +1,9 @@
 from multiprocessing import Process
 import image_preprocess.image_crawling
+import os
 
 image_preprocess.image_crawling.mx_image_count = 400
-image_preprocess.image_crawling.search_terms = ['원빈', '장동건', '강동원', '현빈', '정우성', '송중기', '차은우', '소지섭', '조인성']
+image_preprocess.image_crawling.search_terms = ['류준열', '빅뱅 태양', '임영웅', '광희', '최다니엘', '장도연', 'aoa 지민', '김연아', '채영', '개그우먼 김지민']
 max_process_queue_size = 4
 
 if __name__ == "__main__":
@@ -10,12 +11,13 @@ if __name__ == "__main__":
     job_queue = []
     for search_term in image_preprocess.image_crawling.search_terms:
         if len(job_queue) >= max_process_queue_size:
-            job_queue[0].join()
-            job_queue[1].join()
+            for i in range(max_process_queue_size):
+                job_queue[i].join()
+            job_queue = []
 
         process = Process(target=image_preprocess.image_crawling.crawling_start, args=(search_term,))
         process.start()
         job_queue.append(process)
         print(process)
 
-
+    print("job finished")
